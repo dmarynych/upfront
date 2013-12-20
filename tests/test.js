3840b8a162b8ce9781d0b337fd171a46c59e9352
stark <dmarynych@gmail.com>
2013-12-20 20:20:56 +0200

    travis test 2

--- tests/test.js
+++ tests/test.js
@@ -3,7 +3,7 @@ var expect = require("expect.js");
 describe('Github API', function(){
     describe('getReleases', function() {
         it('should get releases', function() {
-            expect(true).to.be(true);
+            expect(true).to.be(false);
         });
     });
 });
